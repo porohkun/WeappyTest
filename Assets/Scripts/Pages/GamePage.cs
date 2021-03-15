499a1eb7269976a775e0bc483e76c67316b96ab6
poroh-kun <meta.poroh.kun@gmail.com>
2021-03-15 21:14:19 +0300

    character lives

--- Assets/Scripts/Pages/GamePage.cs
+++ Assets/Scripts/Pages/GamePage.cs
@@ -1,7 +1,19 @@
-﻿namespace WeappyTest
+﻿using UnityEngine;
+
+namespace WeappyTest
 {
     [ZenjectBindingInstanceAsSingle]
     public class GamePage : BasePage
     {
+        [SerializeField]
+        private Character.Character _character;
+        [SerializeField]
+        private SpriteRenderer[] _livesSprites;
+
+        private void Update()
+        {
+            for (int i = 0; i < _livesSprites.Length; i++)
+                _livesSprites[i].enabled = i < _character.Context.Lives;
+        }
     }
 }
